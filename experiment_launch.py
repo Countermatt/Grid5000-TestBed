4f3cdd5672e3bd627bbfd871c8e7136d2660bd0e
Matthieu Pigaglio <matthieu.pigaglio@uclouvain.be>
2023-06-27 10:59:58 +0200

    up run and launch script

--- experiment_launch.py
+++ experiment_launch.py
@@ -8,15 +8,18 @@ en.init_logging(level=logging.INFO)
 en.check()
 
 #Change to your Grid5000 user
-nb_node = 5
+nb_node = 180
 arguments = 50  # nb_second
+nb_cpu_node = 18
 network = en.G5kNetworkConf(type="prod", roles=["experiment_network"], site="nancy")
+nb_real_node = nb_node//nb_cpu_node if nb_node%nb_cpu_node == 0 else nb_node//nb_cpu_node + 1
 
+print(nb_real_node)
 conf = (
     en.G5kConf.from_settings(job_name="Louvain-job-1", walltime="0:03:00")
     .add_network_conf(network)
     #.add_machine(roles=["experiment"], cluster="gros", nodes=nb_node-1, primary_network=network)
-    .add_machine(roles=["first"], cluster="gros", nodes=nb_node, primary_network=network)
+    .add_machine(roles=["first"], cluster="gros", nodes=nb_real_node, primary_network=network)
     .finalize()
 )
 
@@ -24,7 +27,7 @@ conf = (
 provider = en.G5k(conf)
 roles, networks = provider.init()
 with en.actions(roles=roles["first"]) as p:
-    p.shell("/home/mapigaglio/run1.sh " + str(arguments) + " " + str(roles["first"][0].address))
+    p.shell("/home/mapigaglio/run1.sh " + str(arguments) + " " + str(roles["first"][0].address) + " " + str(roles["first"][-1].address) + " " + str(nb_node - nb_node//nb_cpu_node) + " " + str(nb_cpu_node))
 
 #with en.actions(roles=roles["experiment"]) as p:
 #    p.shell("/home/mapigaglio/run1.sh "  + str(arguments))
--- run1.sh
+++ run1.sh
@@ -12,8 +12,30 @@ git clone https://github.com/datahop/libp2p-das
 cd libp2p-das
 if hostname = $2
 then
-    ./run.sh bootstrap $1
-else
+    ./run.sh bootstrap $1 $2 &
+    for (( i=0; i<$5-2; i++ ))
+    do
+    ./run.sh $1 &
+    done
     ./run.sh $1
+else
+    if hostname = $3
+    then
+        for (( i=0; i<$4-1; i++ ))
+        do
+        ./run.sh $1 &
+        done
+        ./run.sh $1
+    else
+        for (( i=0; i<$5-1; i++ ))
+        do
+        ./run.sh $1 &
+        done
+        ./run.sh $1
+    fi
 fi
-cp *.csv /home/mapigaglio
+if [!-d /home/mapigaglio/result]; then
+    mkdir -p /home/mapigaglio/result;
+fi;
+mkdir /home/mapigaglio/result/$(date +%d-%m-%y-%H-%M)
+cp *.csv /home/mapigaglio/result/$(date +%d-%m-%y-%H-%M)
