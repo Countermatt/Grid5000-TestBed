8a8d4778225e8d8e32815844793805aaea8551fc
Matthieu Pigaglio <matthieu.pigaglio@uclouvain.be>
2023-06-28 14:05:15 +0200

    resolve ssh crash deploiement script

--- experiment_launch.py
+++ experiment_launch.py
@@ -1,39 +1,71 @@
 import logging
 import enoslib as en
 import os
+import datetime
 
+def add_time(original_time, hours=0, minutes=0, seconds=0):
+    time_delta = datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
+    new_time = original_time + time_delta
+    return new_time
+
+def convert_seconds_to_time(seconds):
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return hours, minutes, seconds
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 en.init_logging(level=logging.INFO)
 en.check()
 
 #Change to your Grid5000 user
-nb_node = 180
-arguments = 50  # nb_second
-nb_cpu_node = 18
-network = en.G5kNetworkConf(type="prod", roles=["experiment_network"], site="nancy")
-nb_real_node = nb_node//nb_cpu_node if nb_node%nb_cpu_node == 0 else nb_node//nb_cpu_node + 1
+login = "mapigaglio"
+nb_node = 1000
+arguments = 600  # nb_second
 
-print(nb_real_node)
+nb_node_per_cpu = nb_node//10
+
+network = en.G5kNetworkConf(type="prod", roles=["experiment_network"], site="nancy")
+
 conf = (
-    en.G5kConf.from_settings(job_name="Louvain-job-1", walltime="0:03:00")
+    en.G5kConf.from_settings(job_name="Louvain-job-1", walltime="0:14:00")
     .add_network_conf(network)
     #.add_machine(roles=["experiment"], cluster="gros", nodes=nb_node-1, primary_network=network)
-    .add_machine(roles=["first"], cluster="gros", nodes=nb_real_node, primary_network=network)
+    .add_machine(roles=["first"], cluster="grisou", nodes=10, primary_network=network)
     .finalize()
 )
 
 # This will validate the configuration, but not reserve resources yet
 provider = en.G5k(conf)
-roles, networks = provider.init()
-with en.actions(roles=roles["first"]) as p:
-    p.shell("/home/mapigaglio/run1.sh " + str(arguments) + " " + str(roles["first"][0].address) + " " + str(roles["first"][-1].address) + " " + str(nb_node - nb_node//nb_cpu_node) + " " + str(nb_cpu_node))
+roles, networks = provider.init(force_deploy=True)
+roles = en.sync_info(roles, networks)
 
+#Network emulation
+netem = en.NetemHTB()
+(
+    netem.add_constraints(
+        src=roles["first"],
+        dest=roles["first"],
+        delay="70ms",
+        rate="1gbit",
+        symmetric=True,)
+)
+
+netem.deploy()
+netem.validate()
+
+with en.actions(roles=roles["first"], on_error_continue=True, background=True) as p:
+    p.shell("/home/" + login + "/run1.sh " + str(arguments) + " " + str(roles["first"][0].address) + " " + str(roles["first"][-1].address) + " " + str(nb_node//10 + nb_node%10) + " " + str(nb_node//10))
 #with en.actions(roles=roles["experiment"]) as p:
 #    p.shell("/home/mapigaglio/run1.sh "  + str(arguments))
 #launch script with list of nodes for arguments
 #results = en.run_command("/home/mapigaglio/run1.sh "  + str(arguments), roles=roles["first"])
 #results = en.run_command("/home/mapigaglio/run2.sh "  + str(arguments), roles=roles["experiments"])
 
+x = datetime.datetime.now()
+h,m,s = convert_seconds_to_time(arguments)
+print("Begin at: ",x)
+print("Expected to finish at: ",add_time(x,h,m,s))
+
 # Release all Grid'5000 resources
-provider.destroy()
+#netem.destroy()
+#provider.destroy()
